8b97ef8d109ed105c833657b389e1fe4e5f1206a
Siva Jagadeesan <sivajag@gmail.com>
2011-07-16 00:48:26 -0700

    added index to daemon

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
-(defproject lein-daemon "0.3.1"
+(defproject org.clojars.sivajag/lein-daemon "0.3.1"
   :description "A lein plugin that daemonizes a clojure process"
   :url "https://github.com/arohner/leiningen"
   :license {:name "Eclipse Public License"}
-  :dependencies [[com.sun.akuma/akuma "1.4"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]])
+  :repositories {"central" "http://repo1.maven.org/maven2"}
+  :dependencies [[com.sun.akuma/akuma "1.4"]
+                 [org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]
+                     [swank-clojure "1.2.1"]])
--- src/leiningen/daemon.clj
+++ src/leiningen/daemon.clj
@@ -19,29 +19,29 @@
       true
       (fail))))
 
-(defn get-pid-path [project alias]
-  (get-in project [:daemon alias :pidfile]))
+(defn get-pid-path [project alias index]
+  (str (get-in project [:daemon alias :pidfile]) "_" index))
 
-(defn pid-present? [project alias]
-  (runtime/get-pid (get-pid-path project alias)))
+(defn pid-present? [project alias index]
+  (runtime/get-pid (get-pid-path project alias index)))
 
-(defn running? [project alias]
-  (runtime/process-running? (pid-present? project alias)))
+(defn running? [project alias index]
+  (runtime/process-running? (pid-present? project alias index)))
 
 (defn inconsistent?
   "true if pid is present, and process not running"
-  [project alias]
-  (and (pid-present? project alias) (not (running? project alias))))
+  [project alias index]
+  (and (pid-present? project alias index) (not (running? project alias index))))
 
 (defn start-main
-  [project alias & args]
+  [project alias index & args]
   (let [ns (get-in project [:daemon alias :ns])
-        timeout (* 5 60)]
-    (if (not (pid-present? project alias))
+        timeout 60]
+    (if (not (pid-present? project alias index))
       (do
         (println "forking" alias)
         (eval-in-project project `(do
-                                    (leiningen.daemon-runtime/init ~(get-pid-path project alias) :debug ~(get-in project [:daemon alias :debug]))
+                                    (leiningen.daemon-runtime/init ~(get-pid-path project alias index) :debug ~(get-in project [:daemon alias :debug]))
                                     ((ns-resolve '~(symbol ns) '~'-main) ~@args))
                          (fn [java]
                            (when (not (get-in project [:daemon alias :debug]))
@@ -51,7 +51,7 @@
                                 (require 'leiningen.daemon-runtime)
                                 (require '~(symbol ns)))
                          true)
-        (wait-for #(running? project alias) #(throwf "%s failed to start in %s seconds" alias timeout) timeout)
+        (wait-for #(running? project alias index) #(throwf "%s failed to start in %s seconds" alias timeout) timeout)
         (println "waiting for pid file to appear")
         (println alias "started")
         (System/exit 0))
@@ -63,25 +63,25 @@
           (println "not starting, pid file present")
           (System/exit 2))))))
 
-(defn stop [project alias]
-  (let [pid (runtime/get-pid (get-pid-path project alias))
+(defn stop [project alias index]
+  (let [pid (runtime/get-pid (get-pid-path project alias index))
         timeout 60]
-    (when (running? project alias)
+    (when (running? project alias index)
       (println "sending SIGTERM to" pid)
       (runtime/sigterm pid))
-    (wait-for #(not (running? project alias)) #(throwf "%s failed to stop in %d seconds" alias timeout) timeout)
-    (-> (get-pid-path project alias) (File.) (.delete))))
+    (wait-for #(not (running? project alias index)) #(throwf "%s failed to stop in %d seconds" alias timeout) timeout)
+    (-> (get-pid-path project alias index) (File.) (.delete))))
 
-(defn check [project alias]
-  (when (running? project alias)
+(defn check [project alias index]
+  (when (running? project alias index)
     (do (println alias "is running") (System/exit 0)))
-  (when (inconsistent? project alias)
+  (when (inconsistent? project alias index)
     (do (println alias "pid present, but NOT running") (System/exit 2)))
   (do (println alias "is NOT running") (System/exit 1)))
 
-(defn check-valid-daemon [project alias]
+(defn check-valid-daemon [project alias index]
   (let [d (get-in project [:daemon alias])
-        pid-path (get-in project [:daemon alias :pidfile])]
+        pid-path (str (get-in project [:daemon alias :pidfile]) "_" index)]
     (when (not d)
       (abort (str "daemon " alias " not found in :daemon section")))
     (when (not pid-path)
@@ -96,28 +96,29 @@ In project.clj, define a keyvalue pair that looks like
  :daemon {:foo {:ns foo.bar
                 :pidfile \"foo.pid\"}}
 
-USAGE: lein daemon start :foo
-USAGE: lein daemon stop :foo
-USAGE: lein daemon check :foo
+USAGE: lein daemon start :foo index
+USAGE: lein daemon stop :foo index
+USAGE: lein daemon check :foo index
 
 this will apply the -main method in foo.bar.
 
 On the start call, additional arguments will be passed to -main
 
-USAGE: lein daemon start :foo bar baz 
+USAGE: lein daemon start :foo index bar baz 
 "
-  [project & [command daemon-name & args :as all-args]]
+  [project & [command daemon-name index  & args :as all-args]]
   (when (or (nil? command)
-            (nil? daemon-name))
+            (nil? daemon-name)
+            (nil? index))
     (abort (help-for "daemon")))
   (let [command (keyword command)
         daemon-name (if (keyword? (read-string daemon-name))
                       (read-string daemon-name)
                       daemon-name)
         alias (get-in project [:daemon daemon-name])]
-    (check-valid-daemon project daemon-name)
+    (check-valid-daemon project daemon-name index)
     (condp = (keyword command)
-      :start (apply start-main project daemon-name args)
-      :stop (stop project daemon-name)
-      :check (check project daemon-name)
+      :start (apply start-main project daemon-name index args)
+      :stop (stop project daemon-name index)
+      :check (check project daemon-name index)
       (abort (str command " is not a valid command")))))
